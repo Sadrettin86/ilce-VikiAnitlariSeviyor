9901a4e41d6dc8627ebacdb9266ed737fc304d0f
Sadrettin86 <ademozcna@gmail.com>
2026-03-06 16:18:42 +0300

    Add Firebase configuration and GeoJSON URLs

---
+++ js/config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCdOtnD9RJRS6sUoUnRDezReMxdBVaud0Q",
+  authDomain: "ilce-vikianitlari.firebaseapp.com",
+  projectId: "ilce-vikianitlari",
+  storageBucket: "ilce-vikianitlari.firebasestorage.app",
+  messagingSenderId: "918748034657",
+  appId: "1:918748034657:web:0648dd55812bc193c71c20"
+};
+
+export const GEOJSON_URL = "https://raw.githubusercontent.com/Sadrettin86/ilce-VikiAnitlariSeviyor/main/districts.geojson";
+export const PROV_URL    = "https://raw.githubusercontent.com/Sadrettin86/ilce-VikiAnitlariSeviyor/main/provinces.geojson";
